14045c9ff09eacb19bd7cecce0a11e85e0fe5930
mikadii <dianacampos2112@gmail.com>
2021-04-03 23:19:30 -0600

    first day of javaScript

---
+++ day1/datatypes.js
@@ -0,0 +1,47 @@
+//5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let lastName= 'Mikadi'; //string
+console.log(typeof(lastName));
+
+let single = false; //boolean
+console.log(typeof(single));
+
+let address = null; //null
+console.log(typeof(address));
+
+let career; //undefined 
+console.log(typeof(career));
+
+console.log(typeof null)
+console.log(typeof 5)
+
+//6. Declare four variables without assigning values
+
+let material;
+let kindof;
+let color;
+let city;
+
+//7. Declare four variables with assigned values
+
+let brand = 'Samsung';
+let size = 5.5;
+let camera = true;
+let memory = null;
+
+//8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = 'Diana'
+let secondName = 'Campos'
+let isMarried = true
+let country = 'Mexico'
+let age = 25
+
+//10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 25
+let yourAge = 30
+
+console.log(`I am ${myAge} years old`)
+console.log(`You are ${yourAge} years old`)
+
---
+++ day1/variable.js
@@ -0,0 +1,22 @@
+//1. Write a single line comment which says, comments can make code readable
+// Comments can make code readable
+
+
+//2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+//3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/*comments can make code readable, 
+easy to reuse and informative */
+
+/*4. Create a variable.js file and declare variables and assign string, 
+boolean, undefined and null data types */
+// Data types
+
+let firstName = 'Diana'; //string
+let single = false; //boolean
+let address = null; //null
+let age; //undefined 
+
+
