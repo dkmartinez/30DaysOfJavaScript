3a9b498f2423bc8d847f15c52b27096629596772
mikadii <dianacampos2112@gmail.com>
2021-04-04 23:26:17 -0500

    practicas del segundo dia

---
+++ day2/exerciseLevel1.js
@@ -0,0 +1,22 @@
+//Exercise: Level 1
+//1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let dayTwo = '30 Days Of JavaScript';
+
+//2. Print the string on the browser console using console.log()
+console.log(dayTwo)
+
+//3.Print the length of the string on the browser console using console.log()
+console.log(dayTwo.length)
+
+//4. Change all the string characters to capital letters using toUpperCase() method
+console.log(dayTwo.toUpperCase())
+
+//5. Change all the string characters to lowercase letters using toLowerCase() method
+console.log(dayTwo.toLowerCase())
+
+//6. Cut (slice) out the first word of the string using substr() or substring() method
+console.log(dayTwo.substr(3,4))
+
+// 7.Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+
+console.log(dayTwo.slice())
