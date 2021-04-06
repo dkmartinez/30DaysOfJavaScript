2a28f060ca94f76d07d962853872a4a6140aa224
mikadii <dianacampos2112@gmail.com>
2021-04-05 23:07:32 -0500

    finalizando cambios dia 2

--- day2/exerciseLevelOne.js
+++ day2/exerciseLevelOne.js
@@ -37,16 +37,33 @@ console.log(companies.split(','))
 // Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
 console.log(dayTwo.replace('JavaScript', 'Python'))
 // What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
-
+console.log(dayTwo.charAt(15))
 // What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+console.log(dayTwo.charCodeAt('J'))
 // Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+console.log(dayTwo.indexOf('30 Days'))
 // Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+console.log(dayTwo.lastIndexOf('JavaScript'))
 // Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let exercise = 'You cannot end a sentence with because because because is a conjunction'
+
+console.log(exercise.indexOf('You cannot'))
 // Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(exercise.lastIndexOf('conjunction'))
 // Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(exercise.search('cannot'))
 // Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+let withSpaces = ' 30 Days Of JavaScript ';
+console.log(withSpaces.trim())
 // Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(dayTwo.startsWith('30'))
 // Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(dayTwo.endsWith('JavaScript'))
 // Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log(dayTwo.match('a'))
 // Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+let incomplete = '30 Days of';
+console.log(incomplete.concat(' JavaScript'))
 // Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log(dayTwo.repeat(2))
+
---
+++ day2/exerciseLevelTwo.js
@@ -0,0 +1,36 @@
+let quoteHolmes = 'There is no exercise better for the heart than reaching down and lifting people up.' 
+console.log(quoteHolmes)
+
+let motherTeresa = "Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log(motherTeresa)
+
+// Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+let numberS = '10'
+numberS = Number(numberS)
+console.log(typeof numberS)
+
+// Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+let notInt = 9.8
+console.log(Math.ceil(notInt))
+
+// Check if 'on' is found in both python and jargon
+
+let searching = ' python and jargon '
+console.log(searching.search('on'))
+// I hope this course is not full of jargon. Check if jargon is in the sentence.
+
+let phrase = 'I hope this course is not full of jargon'
+console.log(phrase.search('jargon'))
+// Generate a random number between 0 and 100 inclusively.
+let numberRand = Math.random()
+let formRand = numberRand * 101
+let arounRand = Math.floor(formRand)
+console.log(arounRand)
+
+// Generate a random number between 50 and 100 inclusively.
+
+// Generate a random number between 0 and 255 inclusively.
+
+// Access the 'JavaScript' string characters using a random number.
+
+// Use console.log() and escape characters to print the following pattern.
