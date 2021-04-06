109af58600fab5c06da0247eb415f224239ad27f
mikadii <dianacampos2112@gmail.com>
2021-04-05 22:35:26 -0500

    finalizando cambios dia 2

--- day2/exerciseLevel1.js
+++ day2/exerciseLevel1.js
@@ -19,4 +19,34 @@ console.log(dayTwo.substr(3,4))
 
 // 7.Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
 
-console.log(dayTwo.slice())
+console.log(dayTwo.substring(3))
+
+//8.Check if the string contains a word Script using includes() method
+
+console.log(dayTwo.includes('Script'))
+//9. Split the string into an array using split() method
+
+console.log(dayTwo.split(' '))
+// Split the string 30 Days Of JavaScript at the space using split() method
+console.log(dayTwo.split(''))
+
+// 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(companies.split(','))
+// Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+console.log(dayTwo.replace('JavaScript', 'Python'))
+// What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+
+// What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+// Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+// Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+// Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+// Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+// Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+// Use match() method to find all the a’s in 30 Days Of JavaScript
+// Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+// Use repeat() method to print 30 Days Of JavaScript 2 times
