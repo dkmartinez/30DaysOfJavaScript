980e022d60d0b8ec8a0f85a2e1041fdd771c4422
mikadii <dianacampos2112@gmail.com>
2021-04-06 19:46:09 -0500

    activities day two 02

--- day2/exerciseLevelThree.js
+++ day2/exerciseLevelThree.js
@@ -11,8 +11,12 @@ console.log(sentence.match(/because/gi))
 const sentence2 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
 let regEx = '%'
+
 //console.log(sentence2.match(regEx))
-console.log(sentence2.replace(regEx, ''))
+console.log(sentence2.replace(regEx,''))
 
 
 // Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+let salary = 5000 + 10000 + 15000
+console.log(salary)
